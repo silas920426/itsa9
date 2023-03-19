49c2c77fe6bbd41e90e3088c439ba0e38c4fe3c0
silas920426 <90045542+silas920426@users.noreply.github.com>
2023-03-19 22:28:12 +0800

    加入專案檔案。

---
+++ itsa9.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>  
+using namespace std;
+int main()
+{
+    int sum = 0, N, i;
+    while (cin >> N) {
+        for (i = 1; i <= N; i++) {
+            if (i % 3 == 0) {
+                sum = sum + i;
+            }
+        }
+        cout << sum << endl;
+    }
+}
